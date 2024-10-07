ed2b7edbec3ab15e6abe8c9e7378e3e63f9b473a
RixarshXD <166057490+RixarshXD@users.noreply.github.com>
2024-10-07 12:26:15 -0300

    pdf dia 2 terminado
    admin.py confiurado

--- PROJECT/APP1/admin.py
+++ PROJECT/APP1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from APP1.models import Empleado
 # Register your models here.
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'email', 'fono']
+
+#registrar los modelos en el admin
+admin.site.register(Empleado, EmpleadoAdmin)
